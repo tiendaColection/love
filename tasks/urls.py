c9d9eda855ac50baac8ac42c8ae21a8866447e75
Mermelada-de-Moras <enigmapacman086@gmail.com>
2024-10-16 07:32:17 -0400

    configuration media-rood

--- tasks/urls.py
+++ tasks/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    from django.conf.urls.static import static
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
