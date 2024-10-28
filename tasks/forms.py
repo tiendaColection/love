af53443f65b44b4a0ebf07607d5910b93ce1b2c1
noequispeliset@gmail.com <noequispeliset@gmail.com>
2024-10-28 17:51:01 -0400

    acction edition figurine 0.4

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,6 +5,11 @@ class FigurineForm(forms.ModelForm):
     class Meta:
         model = Figurine
         fields = ['name', 'description', 'category', 'genre', 'imagen', 'price', 'is_sold']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control','placeholder': 'Nombre'}),
+            'description':forms.Textarea(attrs={'class': 'form-control', 'placeholder':'Descripcion de la figura'}),
+            'imagen': forms.ClearableFileInput(attrs={'class': 'form-control-file'})
+        }
         
 
 class GenreFrom(forms.ModelForm):
