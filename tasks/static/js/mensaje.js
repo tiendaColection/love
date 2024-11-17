deca8eabf78cbd5476912ca1a48f29628e7d68b2
carolinaNoe <carolinanoequispe777@gmail.com>
2024-11-17 06:48:17 -0400

    repare index message

--- tasks/static/js/mensaje.js
+++ tasks/static/js/mensaje.js
@@ -1,4 +1,9 @@
 window.onload = function() {
     const banner = document.getElementById("nav-banner");
     banner.style.display = "block";
+
+    // Desaparecer al hacer clic
+    banner.onclick = function() {
+        banner.style.display = "none";
+    };
 };
