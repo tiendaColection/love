c05e36395beaa07448ae9727bfda7690edc53925
carolinaNoe <carolinanoequispe777@gmail.com>
2024-11-22 00:27:00 -0400

    nueva actualizacion

--- tasks/static/js/mensaje.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    const banner = document.getElementById("nav-banner");
-    banner.style.display = "block";
-
-    // Desaparecer después de 5 segundos (5000 ms)
-    setTimeout(function() {
-        banner.style.display = "none";
-    }, 5000);
-};
