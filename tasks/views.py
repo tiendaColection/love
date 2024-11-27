26e58d7a3c2097de50533e71b12ab150bf21ac19
carolinaNoe <carolinanoequispe777@gmail.com>
2024-11-27 00:00:04 -0400

    login get up

--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from .models import Category, Figurine
 from .forms import FigurineForm, GenreFrom, CategoryFrom
-import logging
 
 def index(request):
     ofertas = 120
